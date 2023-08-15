fe503b2fa2e27f3edfdbbfab95c6e344cd931b04
79855 <leb.alex123@gmail.com>
2023-08-15 23:54:47 +0300

    3.1. Массив с пустыми ячейками [#176470]

---
+++ src/main/java/ru/job4j/pojo/Product.java
@@ -0,0 +1,11 @@
+package ru.job4j.pojo;
+
+public class Product {
+    private final String name;
+    private final int count;
+
+    public Product(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+}
