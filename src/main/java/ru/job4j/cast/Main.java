ed1e3954baa35eaa061b186e0196fa1261c008f9
79855 <leb.alex123@gmail.com>
2023-08-27 16:22:10 +0300

    5.1. Зачем нужно приведение типов [#310019]

---
+++ src/main/java/ru/job4j/cast/Main.java
@@ -0,0 +1,17 @@
+package ru.job4j.cast;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Vehicle airplane = new Airplane();
+        Vehicle bus = new Bus();
+        Vehicle train = new Train();
+
+        Vehicle[] vehicles = {airplane, bus, train};
+
+        for (Vehicle vehicle : vehicles) {
+            vehicle.move();
+        }
+    }
+}
+
