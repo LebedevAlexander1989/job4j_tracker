4a03f49ab8b45de08d966e935f0495cdb7d5fea8
79855 <leb.alex123@gmail.com>
2023-09-03 23:22:08 +0300

    7. Статические методы. [#181779 #440199]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -50,7 +50,7 @@ public class StartUI {
         }
     }
 
-    public void editItem(Input input, Tracker tracker) {
+    public static void editItem(Input input, Tracker tracker) {
         System.out.println("=== Edit item ===");
         int id = input.askInt("Enter id: ");
         String name = input.askStr("Enter name: ");
