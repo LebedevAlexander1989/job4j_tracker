bd2fd537e08e84938f4699f9c96c2cefc8ae475c
79855 <leb.alex123@gmail.com>
2024-10-10 13:45:12 +0300

    2. Интерфейс Deque [#504943 #516486]

--- src/main/java/ru/job4j/queue/ReconstructPhrase.java
+++ src/main/java/ru/job4j/queue/ReconstructPhrase.java
@@ -1,7 +1,6 @@
 package ru.job4j.queue;
 
 import java.util.Deque;
-import java.util.Iterator;
 
 public class ReconstructPhrase {
 
@@ -28,9 +27,8 @@ public class ReconstructPhrase {
 
     private String getDescendingElements() {
         StringBuilder builder = new StringBuilder();
-        Iterator<Character> iterator = descendingElements.descendingIterator();
-        while (iterator.hasNext()) {
-            builder.append(iterator.next());
+        while (descendingElements.size() > 0) {
+            builder.append(descendingElements.pollLast());
         }
         return builder.toString();
     }
