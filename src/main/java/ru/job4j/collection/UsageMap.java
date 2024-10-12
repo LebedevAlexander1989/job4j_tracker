2bdd35113337e5b04ac0a387cc7abf189fc469a3
79855 <leb.alex123@gmail.com>
2024-10-12 22:02:39 +0300

    0. Map, HashMap [#212652]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,9 @@ public class UsageMap {
     public static void main(String[] args) {
         Map<String, String> map = new HashMap<>();
         map.put("lebedev@yandex.ru", "Alexander Lebedev");
+        map.put("lebedev@yandex.ru", "Alexander Leb");
+        map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("sergivanov@yandex.ru", "Sergey Ivanov");
 
         for (String k : map.keySet()) {
             System.out.println("email: " + k + ", name: " + map.get(k));
